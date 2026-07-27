5f3dc22b36b01641a03b04b72346d7ef3e034035
richardronyo <62360494+richardronyo@users.noreply.github.com>
2026-07-27 15:07:01 -0600

    OEC Interface Complete

--- oec.py
+++ oec.py
@@ -33,7 +33,7 @@ class OECClient:
     def print_cubes(self):
         count = 0
         for cube_name in self._cubes:
-            print(f'{count + 1}.\t{cube_name}')
+            print(f'\t{count + 1}. {cube_name}')
             count += 1
 
     def get_cube_schema(self, cube_number):
@@ -51,8 +51,28 @@ class OECClient:
 
         return schema
 
-    def set_cube_data(self, cube_number, drilldowns, measures, include = None, cube_type = 'jsonrecords'):
+    def get_cube_members(self, cube_number, level):
         cube_name = self._cubes[cube_number - 1]
+        parameters = {
+            'cube': cube_name,
+            'level': level
+        }
+
+        response = requests.get(f'{OEC_URL}/members', params = parameters)
+
+        if response.status_code != 200:
+            print("Request failed")
+            print("Status: ", response.status_code)
+            print("Response: ", response.text)
+
+            return
+
+        members = response.json()
+
+        return members
+        
+    def set_cube_data(self, cube_number, drilldowns, measures, include = None, cube_type = 'jsonrecords'):
+        cube_name = self._cubes[cube_number]
         if include:
             parameters = {
                     'cube': cube_name,
@@ -77,16 +97,75 @@ class OECClient:
             return
 
         dataset = response.json()
+        print(dataset)
         self._dataset = pd.DataFrame(dataset['data'])
         return    
 
     def get_cube_data(self):
         return self._dataset
 
+def print_dic(dictionary):
+    for key, value in dictionary.items():
+        print(key)
 
-if __name__ == "__main__":
+        if type(value) == dict:
+            print_dic(value)
+        if type(value) == list:
+            for entry in value:
+                print_dic(entry)
+        else:
+            print(f'\t{value}')
+
+
+def main():
+    """
+    This will act as the interface for the OEC API 
+    """
     oec = OECClient()
-    print(oec.get_cube_names())
-    oec.set_cube_data(2, "Time,Currency", "Rate")
-    data = oec.get_cube_data()
-    print(data.head(20))
+
+    print("Welcome to the OEC API Inteface!")
+    running = True
+    while running:
+        print("---------------------------------------------------------------------")
+        option = input("Select an option\n\t1. View Available Datasets\n\t2. View Schema\n\t3. Get Dataset\n\t4. Get Members\n\t5. Save Dataset\n\t6. Exit\nEnter your choice: ") 
+        if int(option) == 1:
+            print("Available Datasets")
+            oec.print_cubes()
+
+        if int(option) == 2:
+            cube = input("\tDataset Number: ")
+            schema = oec.get_cube_schema(int(cube))
+            print_dic(schema)
+
+        if int(option) == 3:
+            cube_number = input("Enter the cube number: ")
+            cube_number = int(cube_number) - 1
+            drilldowns = input("\tEnter Drilldowns: ")
+            measures = input("\tEnter Measures: ")
+            include = input("Enter Include: ")
+
+            print(f'Getting dataset: {oec.get_cube_names()[cube_number]}')
+            oec.set_cube_data(cube_number, drilldowns = drilldowns, measures = measures, include = include)
+
+        if int(option) == 4:
+            cube_number = input("Enter the cube number: ")
+            cube_number = int(cube_number) - 1
+            print(f'Accessing cube {oec.get_cube_names()[cube_number]}') 
+            level = input("Enter the level: ")
+            member = oec.get_cube_members(cube_number + 1, level)
+
+            print(member)
+
+        if int(option) == 5:
+            filename = input("\tEnter the filename: ")
+            dataset = oec.get_cube_data()
+            dataset.to_csv(f'data/{filename}.csv')
+
+
+        if int(option) == 6:
+            print("Thank You!")
+            running = False
+
+    return 
+if __name__ == "__main__":
+    main()
